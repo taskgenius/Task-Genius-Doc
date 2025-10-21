71554621e6688ed9c7bcf943b7aa5bacf0dc3d98
Quorafind <bonopengate@gmail.com>
2025-10-21 11:23:31 +0800

    chore: update changelog

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -2,14 +2,16 @@ import defaultMdxComponents from "fumadocs-ui/mdx";
 import type { MDXComponents } from "mdx/types";
 import { Mermaid } from "@/components/mdx/mermaid";
 import { DeepLinkButton, DeepLinkFallback } from "@/components/DeepLinkButton";
+import { ImageZoom } from "fumadocs-ui/components/image-zoom";
 
 // use this function to get MDX components, you will need it for rendering MDX
 export function getMDXComponents(components?: MDXComponents): MDXComponents {
-  return {
-    ...defaultMdxComponents,
-    Mermaid,
-    DeepLinkButton,
-    DeepLinkFallback,
-    ...components,
-  };
+	return {
+		...defaultMdxComponents,
+		Mermaid,
+		DeepLinkButton,
+		DeepLinkFallback,
+		img: (props) => <ImageZoom {...(props as any)} />,
+		...components,
+	};
 }
